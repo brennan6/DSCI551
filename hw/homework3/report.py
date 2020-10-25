344662c9e944853fe800f902ce0bd8eb0f78bf58
Matthew Brennan <mbrennan@xactlycorp.com>
2020-10-25 15:01:13 -0600

    Fixed all mistakes

--- hw/homework3/report.py
+++ hw/homework3/report.py
@@ -7,10 +7,10 @@ date = sys.argv[1]
 output_file_name = sys.argv[2]
 
 cnx = mysql.connector.connect(user = 'dsci551', password = 'dsci551', database = 'covid19')
+output_dict = {}
 
 def create_report(date, output_file_name):
     global cnx
-
     date_formatted = date[3:] + '-' + date[0:2] + '%'
     prev_month = int(date[0:2])
     if prev_month < 10:
@@ -20,39 +20,57 @@ def create_report(date, output_file_name):
     previous_date_formatted = date[3:] + '-' + prev_month_str + '%'
 
     args = [date_formatted, previous_date_formatted, date_formatted, previous_date_formatted, previous_date_formatted]
-    #practice = "select date, sex, confirmed, deceased from (select * from timegender where date like '2020-03%' OR date like '2020-02%') as t where date >= ALL (select date from timegender where date like '2020-03%') OR (date like '2020-02%' and date >= ALL (select date from timegender where date like '2020-02%'));"
+    
+    # Gender Query:
     cursor = cnx.cursor()
     gender_query = ("select date, sex, confirmed, deceased from (select * from timegender where date like '{}' OR date like '{}') as t" +
             " where date >= ALL (select date from timegender where date like '{}')" +
             " OR (date like '{}' and date >= ALL (select date from timegender where date like '{}')) order by date desc;").format(*args)
     cursor.execute(gender_query)
     vals = cursor.fetchall()
-    #format_dict(vals)
+    format_dict(vals, "gender")
+    cursor.close()
 
+    # Age Query:
+    cursor = cnx.cursor()
+    age_query = ("select date, age, confirmed, deceased from (select * from timeage where date like '{}' OR date like '{}') as t" +
+            " where date >= ALL (select date from timeage where date like '{}')" +
+            " OR (date like '{}' and date >= ALL (select date from timeage where date like '{}')) order by date desc;").format(*args)
+    cursor.execute(age_query)
+    vals = cursor.fetchall()
+    format_dict(vals, "age")
+    cursor.close()
 
-    for row in vals:
-        print(row)
+    # Province Query:
+    cursor = cnx.cursor()
+    province_query = ("select date, province, confirmed, deceased from (select * from timeprovince where date like '{}' OR date like '{}') as t" +
+            " where date >= ALL (select date from timeprovince where date like '{}')" +
+            " OR (date like '{}' and date >= ALL (select date from timeprovince where date like '{}')) order by date desc;").format(*args)
+    cursor.execute(province_query)
+    vals = cursor.fetchall()
+    format_dict(vals, "province")
+    cursor.close()
 
-
+    with open(output_file_name, 'w') as fp:
+        json.dump(output_dict, fp)
     return
 
-def format_dict(vals):
-    output_dict = {}
+def format_dict(vals, key):
+    global output_dict
     group_dict = {}
     for row in vals:
         cd_dict = {}
         date, group, confirmed, deceased = row[0], row[1], row[2], row[3]
         cd_dict["confirmed"] = confirmed
         cd_dict["deceased"] = deceased
-        if group in group_dict:
+        if group not in group_dict:
             group_dict[group] = cd_dict
         else:
             group_dict[group]["confirmed"] = group_dict[group]["confirmed"] - cd_dict["confirmed"]
             group_dict[group]["deceased"] = group_dict[group]["deceased"] - cd_dict["deceased"]
         
-    output_dict["gender"] = group_dict
-    with open(output_file_name, 'w') as fp:
-        json.dump(output_dict, fp)
+    output_dict[key] = group_dict
+    return
 
 create_report(date, output_file_name)
     
