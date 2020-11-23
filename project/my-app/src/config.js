35c0725b8b6959ceffc61a48a8fd8037631002bf
Matthew Brennan <mbrennan@xactlycorp.com>
2020-11-23 14:31:43 -0800

    Deleted broken npm

--- project/my-app/src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    apiKey: "AIzaSyDlIxWt4I9NQX6jZ1qVm_rtm-kEgBeE3Dg",
-    projectId: "project-dsci551",
-    databaseURL: "https://project-dsci551.firebaseio.com"
-}
-  
-export default config;
